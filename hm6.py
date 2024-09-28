26c34980e35d7c6748845eb1104755eb6fa8b052
AntiKward <antikward@gmail.com>
2024-09-28 16:20:37 +0600

    'hm4'

--- hm6.py
+++ hm6.py
@@ -1,7 +1,16 @@
-# def is_even(a):
-#   if a % 2 == 0:
-#     print('Число чётное')
-#   else:
-#     print('Нечётное число')
-# is_even(int(input('')))
+def is_even(a):
+  if a % 2 == 0:
+    print(f'{a} True')
+  else:
+    print(f'{a} False')
+is_even(int(input('')))
 
+def chet_chis():
+  a = int([1,2,3,4,5,6,7,8,9,10])
+  if a % 2 == 0:
+    print('чётное число')
+  else:
+    print('')
+    
+    
+chet_chis()
